c401a446499c1f5cd85750f181466f94f4821679
Avinash Kumar <assistavinash@gmail.com>
2026-02-24 17:06:58 +0530

    Fix route handler to match all paths in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
-app.get("*name", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
