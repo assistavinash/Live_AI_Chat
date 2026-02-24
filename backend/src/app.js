e0f5ef5dcc256214f82548869a478b3287aefaf6
Avinash Kumar <assistavinash@gmail.com>
2026-02-24 17:09:13 +0530

    Fix file path in route handler for serving index.html

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 module.exports = app;
